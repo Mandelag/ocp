4d7dfbdcca7f7dcc4e8896bde2cf894d44221f7c
Keenan Gebze <keenan@bahasa.ai>
2018-10-16 00:21:33 +0700

    Initial commit, created Schedule class.

---
+++ chapter5/src/main/java/com/mandelag/ocp/chapter5/Schedule.java
@@ -0,0 +1,42 @@
+package com.mandelag.ocp.chapter5;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.time.Duration;
+
+/**
+ * Represent contiguous block of time in a day.
+ */
+public class Schedule {
+  private final DayOfWeek dayOfWeek;
+  private final LocalTime startTime;
+  private final LocalTime endTime;
+
+  public Schedule(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+    this.dayOfWeek = dayOfWeek;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
+
+  public Duration getDuration() {
+    return Duration.between(startTime, endTime);
+  }
+
+  public boolean isOverlaping(Schedule that) {
+    return this.getDayOfWeek() == that.getDayOfWeek() && 
+      this.getStartTime().isBefore(that.getEndTime()) &&
+      this.getEndTime().isAfter(that.getStartTime());
+  }
+
+  public DayOfWeek getDayOfWeek() {
+    return this.dayOfWeek;
+  }
+
+  public LocalTime getStartTime() {
+    return this.startTime;
+  }
+
+  public LocalTime getEndTime() {
+    return this.endTime;
+  }
+}
